56d8b2b675e980c5f5ee768067a12c2a3b06bb72
Mr Game Pro <34198694+MrGameMemo@users.noreply.github.com>
2019-08-08 16:04:55 +0200

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,8 @@
+# Intro (Leçon 1) - Introduction à Python | Tous droits réservés - Kodin School 2019
+
+#-----------------------------------------------------------------------------------------#
+
+print("J'apprends Python avec Kodin School")
+
+# Ouvrez un terminal (Ctrl + ù) puis tapez "python3 leçon2.py" pour éxecuter ce programme.
+# Si vous voyez affiché "J'apprends Python avec Kodin School" affiché dans la console, vous avez réussi à exécuter ce programme.
