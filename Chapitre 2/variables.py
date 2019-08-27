6a901525dc334c0c5ffac9777387479580ccc00b
Mr Game Pro <34198694+MrGameMemo@users.noreply.github.com>
2019-08-27 21:16:48 +0200

    Add files via upload

--- Chapitre 2/variables.py
+++ Chapitre 2/variables.py
@@ -2,3 +2,11 @@
 
 #-----------------------------------------------------------------------------------------#
 
+message = "J'apprends Python avec Kodin School !" # Déclaration d'une variable
+
+print(message) # Le mot clé "message" contient maintenant le message : "J'apprends Python avec Kodin School !"
+
+message = "Je viens de modifier le contenu de la variable"
+
+# Ouvrez un terminal (Ctrl + ù) puis tapez "python3 leçon2.py" pour éxecuter ce programme.
+# Si vous voyez affiché "J'apprends Python avec Kodin School" affiché dans la console, vous avez réussi à exécuter ce programme.
